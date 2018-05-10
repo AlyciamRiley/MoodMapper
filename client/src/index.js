c432b27a05ea239370a6c00ab28f539878219c82
Ben Cross <bencross323@gmail.com>
2018-05-09 19:57:06 -0500

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,9 +5,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-<<<<<<< Updated upstream
 ReactDOM.render(<App />, document.getElementById("root"));
-=======
 ReactDOM.render(<Main />, document.getElementById("root"));
->>>>>>> Stashed changes
 registerServiceWorker();
