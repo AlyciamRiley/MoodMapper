70aa8305cc59db17dd35d3d760878e054e333009
Ben Cross <bencross323@gmail.com>
2018-05-09 20:05:21 -0500

    updated index.js

--- client/src/index.js
+++ client/src/index.js
@@ -6,5 +6,4 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-ReactDOM.render(<Main />, document.getElementById("root"));
 registerServiceWorker();
